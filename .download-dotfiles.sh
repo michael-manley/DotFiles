48dbf0eeed18c8458c564416c51901efb58c7a04
Michael Manley <mjmanley@ntge.net>
2023-05-15 16:00:49 -0700

    Update .download-dotfiles.sh

--- .download-dotfiles.sh
+++ .download-dotfiles.sh
@@ -8,8 +8,8 @@ config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    echo "Deleting original files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} rm -rf {}
 fi;
 config checkout
 config config status.showUntrackedFiles no
